a8b4f738c36761a17396894466ecfb0cabe28d53
samiullah <samireshi002@gmail.com>
2022-05-02 04:35:28 +0530

    Time: 178 ms (22.27%), Space: 53.3 MB (83.57%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -4,25 +4,23 @@
  */
 var productExceptSelf = function(nums) {
     
-    let leftproductArray=[]
-leftproductArray[0]=1
-let rightproductArray = []
-rightproductArray[nums.length-1]=1
-let result = []
-
-
-
- for(i=1;i<nums.length;i++){
-     leftproductArray[i]=leftproductArray[i-1]*nums[i-1]
- }
-
- for(i=nums.length-2;i>=0;i--){
-    rightproductArray[i]=rightproductArray[i+1]*nums[i+1]
-}
-
-for(i=0;i<nums.length;i++){
-     result[i]=leftproductArray[i]*rightproductArray[i]
-}
+    let leftProductArray =[]
+    leftProductArray[0]=1
+    let rightProductArray = []
+    rightProductArray[nums.length-1]=1
+    let result =[]
+    
+    for(i=1;i<nums.length;i++){
+        leftProductArray[i]=leftProductArray[i-1]*nums[i-1]
+    }
+    
+    for(i=nums.length-2;i>=0;i--){
+        rightProductArray[i] = rightProductArray[i+1]*nums[i+1]
+    }
+    
+    for(i=0;i<nums.length;i++){
+        result[i]= leftProductArray[i]*rightProductArray[i]
+    }
     
     return result
     
